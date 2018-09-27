532da8db63cbebd4158975c97ca153af7cfaee44
benjamin <benjamin.lindqvist@gmail.com>
2018-09-28 00:00:49 +0200

    more counsel-map

--- lisp/counsel-bar.el
+++ lisp/counsel-bar.el
@@ -10,6 +10,8 @@
   "Speedbar window.")
 (defvar counsel-bar-mode-map (make-sparse-keymap)
   "Map for `nav-mode'.")
+(defvar counsel-bar-active-map (make-sparse-keymap)
+  "Active minor mode map.")
 
 (define-minor-mode counsel-bar-mode
   "Do some shit. "
@@ -17,21 +19,33 @@
   :global nil
   :keymap counsel-bar-mode-map
   (if counsel-bar-mode
-      (progn nil)
+      (progn )
+    ;;disable
+    ))
+
+(define-minor-mode counsel-bar-active-mode
+  "Do some shit. "
+  :group 'counsel-bar
+  :global nil
+  :keymap counsel-bar-active-map
+  (if counsel-bar-active-mode
+      (progn )
     ;;disable
     ))
 
 (defvar counsel-bar-candidates nil)
 (make-variable-buffer-local 'counsel-bar-candidates)
 
-(defcustom counsel-bar-filter-regex "^\\*" "Fuasfasf." :group 'counsel-bar :type 'string)
-(setq counsel-bar-filter-regex "^\\*")
+(defcustom counsel-bar-filter-regex "^\\*"
+  "Fuasfasf."
+  :group 'counsel-bar
+  :type 'string)
 
 (defun counsel-bar--insert (candidate &optional number)
   "Insert CANDIDATE unless it satifies predicate, NUMBER too."
   (if number (insert (format " %-3s" number)))
-  (insert (propertize (concat candidate "\n") 'face font-lock-variable-name-face))
-  )
+  (insert (propertize (concat candidate "\n")
+                      'face font-lock-variable-name-face)))
 
 (defun counsel-bar--filter-candidate (candidate)
   "Ass fff aa CANDIDATE."
@@ -49,27 +63,27 @@
                      (selected-window)
                      counsel-bar-width
                      t)))
-    (switch-to-buffer (concat "*counsel-bar*<" (buffer-name) ">"))
-    (insert (propertize (concat "-- " (projectile-project-name) " --\n") 'face font-lock-keyword-face))
+    (switch-to-buffer (concat "*counsel-bar*<" (projectile-project-name) ">"))
+    (insert (propertize (concat "-- " (projectile-project-name) " --\n\n")
+                        'face font-lock-keyword-face))
     (mapcar* 'counsel-bar--insert
              (sort tmp-counsel-bar-candidates 'string<)
              (number-sequence 1 (length tmp-counsel-bar-candidates)))
+    (counsel-bar-active-mode)
 
-    (unless counsel-bar-right-side (other-window 1)) ;; hacky :(
-    ))
+    (unless counsel-bar-right-side (other-window 1))))
 
 (global-set-key (kbd "C-2") 'counsel-bar-open-window)
 
-(global-set-key (kbd "M-s 1 RET") (lambi (when counsel-bar-candidates (switch-to-buffer (nth 0 counsel-bar-candidates )))))
-(global-set-key (kbd "M-s 2") (lambi (switch-to-buffer (nth 1 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 3") (lambi (switch-to-buffer (nth 2 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 4") (lambi (switch-to-buffer (nth 3 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 5") (lambi (switch-to-buffer (nth 4 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 6") (lambi (switch-to-buffer (nth 5 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 7") (lambi (switch-to-buffer (nth 6 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 8") (lambi (switch-to-buffer (nth 7 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 9") (lambi (switch-to-buffer (nth 8 counsel-bar-candidates ))))
-(global-set-key (kbd "M-s 10 RET") (lambi (switch-to-buffer (nth 9 counsel-bar-candidates ))))
+(mapc
+ (lambda (x)
+   (funcall
+    'define-key counsel-bar-mode-map
+    (kbd (concat "M-s " (number-to-string x) " RET"))
+    `(lambda () (interactive)
+      (switch-to-buffer (nth (- ,x 1) counsel-bar-candidates)))))
+ (number-sequence 1 70))
+
 
 (provide 'counsel-bar)
 ;;; counsel-bar.el ends here
